f82ceaf88a7bfaeb186025d52246e27995143d57
Suncraft <cookies666@msn.com>
2021-01-14 00:59:44 +0000

    Pair programming fixes

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0)return undefined;
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const tempArr = letters[0].map((col, i) => letters.map(row => row[i]));
+    const verticalJoin = tempArr.map(ls => ls.join(''));
+    const reversedWord = word.split('').reverse().join('');
+    for ( let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } else if (l.includes(reversedWord)){
+            return true;
+        }
+    for (let x of verticalJoin) {
+        if (x.includes(word)) {
+            return true;
+        } else if (x.includes(reversedWord)){
+            return true;
+        }  
     }
+        }
+        return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
